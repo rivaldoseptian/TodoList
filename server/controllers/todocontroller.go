18cc0473b5ac9d2ab80ada770e3651def1b61f43
rivaldoseptian <rivaldo.septian121@gmail.com>
2023-05-11 10:57:46 +0700

    feat:post, put, delete todo

--- server/controllers/todocontroller.go
+++ server/controllers/todocontroller.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"server/config"
 	"server/helpers"
 	"server/models"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 func GetAllTodo(w http.ResponseWriter, r *http.Request) {
@@ -21,4 +28,115 @@ func GetAllTodo(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	helpers.Response(w, 200, "Success", todo)
+}
+
+func GetOneTodo(w http.ResponseWriter, r *http.Request) {
+	idTodo := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idTodo)
+
+	var todo models.Todo
+
+	if err := config.DB.First(&todo, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helpers.Response(w, 404, fmt.Sprintf("Todo with ID %v Not Found", id), nil)
+			return
+		}
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+	helpers.Response(w, 200, "Success", todo)
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	var todo models.Todo
+
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	defer r.Body.Close()
+
+	if todo.ActivityGroupID == 0 {
+		helpers.Response(w, 400, "activity_group_id cannot be null", nil)
+		return
+	}
+
+	if todo.Title == "" {
+		helpers.Response(w, 400, "title cannot be null", nil)
+		return
+	}
+	if todo.Priority == "" {
+		helpers.Response(w, 400, "priority cannot be null", nil)
+		return
+	}
+
+	if err := config.DB.Create(&todo).Error; err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+	helpers.Response(w, 201, "Success", todo)
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	idTodo := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idTodo)
+
+	var todo models.Todo
+
+	if err := config.DB.First(&todo, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helpers.Response(w, 404, fmt.Sprintf("Todo with ID %v Not Found", id), nil)
+			return
+		}
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	defer r.Body.Close()
+
+	if todo.ActivityGroupID == 0 {
+		helpers.Response(w, 400, "activity_group_id cannot be null", nil)
+		return
+	}
+
+	if todo.Title == "" {
+		helpers.Response(w, 400, "title cannot be null", nil)
+		return
+	}
+	if todo.Priority == "" {
+		helpers.Response(w, 400, "priority cannot be null", nil)
+		return
+	}
+
+	if err := config.DB.Where("todo_id = ? ", id).Updates(&todo).Error; err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+	helpers.Response(w, 200, "Success", todo)
+
+}
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	idTodo := mux.Vars(r)["id"]
+	todo_id, _ := strconv.Atoi(idTodo)
+
+	var todo models.Todo
+
+	res := config.DB.Delete(&todo, todo_id)
+
+	if res.Error != nil {
+		helpers.Response(w, 500, res.Error.Error(), nil)
+		return
+	}
+
+	if res.RowsAffected == 0 {
+		helpers.Response(w, 404, fmt.Sprintf("Todo with ID %v Not Found", todo_id), nil)
+		return
+	}
+	helpers.Response(w, 200, "Success", nil)
 }
--- server/routes/todo.go
+++ server/routes/todo.go
@@ -9,8 +9,8 @@ import (
 func TodoRouter(r *mux.Router) {
 	router := r.PathPrefix("/todo-items").Subrouter()
 	router.HandleFunc("", controllers.GetAllTodo).Methods("GET")
-	// router.HandleFunc("", controllers.CrateActivity).Methods("POST")
-	// router.HandleFunc("/{id}", controllers.GetOne).Methods("GET")
-	// router.HandleFunc("/{id}", controllers.UpdateActivity).Methods("PUT")
-	// router.HandleFunc("/{id}", controllers.DeleteActivity).Methods("DELETE")
+	router.HandleFunc("", controllers.CreateTodo).Methods("POST")
+	router.HandleFunc("/{id}", controllers.GetOneTodo).Methods("GET")
+	router.HandleFunc("/{id}", controllers.UpdateTodo).Methods("PUT")
+	router.HandleFunc("/{id}", controllers.DeleteTodo).Methods("DELETE")
 }
